08667a5f30bbd2832e01669144c3122481d87f01
Toms Audrins <toms.audrins1@falck.com>
2023-06-24 01:40:58 +0200

    Increase default padding to 12 px

--- js/updateViewFunctions.ts
+++ js/updateViewFunctions.ts
@@ -17,7 +17,7 @@ export const updateDynamicStyles = (context: ComponentFramework.Context<IInputs>
     inputElement.setAttribute("placeholder", context.parameters.placeholderText.raw || "Type a new message");
     inputElement.style.fontFamily = context.parameters.fontFamily.raw || "Segoe UI, sans-serif";
     inputElement.style.borderRadius = `${context.parameters.borderRadius.raw || 4}px`;
-    inputElement.style.padding = `${context.parameters.padding.raw || 16}px`;
+    inputElement.style.padding = `${context.parameters.padding.raw || 12}px`;
     inputElement.style.fontSize = `${context.parameters.fontSize.raw || 14}px`;
     inputElement.maxLength = context.parameters.maxLength.raw || 1000;
     
